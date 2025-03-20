1324755706fbbc4e151ead385e346868ea10a3b3
FelixGustav <guto.felix2003@gmail.com>
2025-03-20 18:08:31 -0300

    repetition structure

---
+++ atividades/aula55/script.js
@@ -0,0 +1,20 @@
+const pessoa = {
+    nome: 'g',
+    sobrenome: 'fr',
+    idade: 22
+}
+
+// console.log('in')
+// for(let i in pessoa){
+//     console.log(pessoa[i])
+//     }
+// console.log("###");
+
+
+
+const frutas = ["maça", "banana","pera"]
+    console.log('of')
+for(let i of frutas){
+    console.log(i)
+   
+}
